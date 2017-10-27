500feb1b995211299d62f45c9b00b5b11d3bddfe
mmmcnul <m.mcnully13@gmail.com>
2017-10-27 09:53:15 -0500

    Added check for parent

--- KA-Notifications/SI-parentChildKAOwner.js
+++ KA-Notifications/SI-parentChildKAOwner.js
@@ -11,24 +11,25 @@ parentChildKAOwner.prototype = {
 			var parent_to_KA;
 			while (gr.next()) {
 				parent_to_KA = gr.parent.short_description;
+			}//while
+			if (parent_to_KA) {
 				gs.info('Parent to '+ number +' : '+ parent_to_KA);
-			}//while
-
-			var gr_owners = new GlideRecord('kb_knowledge');
-			gr_owners.addQuery('parent.short_description',parent_to_KA);
-			gr_owners.query();
-			gs.info('Parent count : '+ gr_owners.getRowCount() );
-			var owner_group=[];
-			while (gr_owners.next()) {
-				var key = gr_owners.u_article_owner.name.trim();
-				if (key) {
-					owner_group.push(key);
+				var gr_owners = new GlideRecord('kb_knowledge');
+				gr_owners.addQuery('parent.short_description',parent_to_KA);
+				gr_owners.query();
+				gs.info('Parent count : '+ gr_owners.getRowCount() );
+				var owner_group=[];
+				while (gr_owners.next()) {
+					var key = gr_owners.u_article_owner.name.trim();
+					if (key) {
+						owner_group.push(key);
+					}
+				}//while
+				var unique_owner_group = new ArrayUtil().unique(owner_group);
+				gs.info('groups : '+ unique_owner_group.length);
+				for (var i=0; i < unique_owner_group.length; i++){
+					gs.info(unique_owner_group[i]);
 				}
-			}//while
-			var unique_owner_group = new ArrayUtil().unique(owner_group);
-			gs.info('groups : '+ unique_owner_group.length);
-			for (var i=0; i < unique_owner_group.length; i++){
-				gs.info(unique_owner_group[i]);
 			}
 		}//try
 		catch(ex) {
